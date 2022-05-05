2c9071725de269681d43615c650ebda49efdea8e
Jungle <1198581227@qq.com>
2022-05-05 22:55:24 +0800

    移除异常数据

--- scratch/statistic.py
+++ scratch/statistic.py
@@ -151,3 +151,12 @@ def correlation(x, y: List[float]) -> float:
 
 assert 0.24 < correlation(num_friends, daily_minutes) < 0.25
 assert 0.24 < correlation(num_friends, daily_hours) < 0.25
+
+# 去除掉异常值
+outlier = num_friends.index(100)
+num_friends_good = [x for i, x in enumerate(num_friends) if i != outlier]
+daily_minutes_good = [x for i, x in enumerate(daily_minutes) if i != outlier]
+daily_hours_good = [dm / 60 for dm in daily_minutes_good]
+
+assert 0.57 < correlation(num_friends_good, daily_minutes_good) < 0.58
+assert 0.57 < correlation(num_friends_good, daily_hours_good) < 0.58

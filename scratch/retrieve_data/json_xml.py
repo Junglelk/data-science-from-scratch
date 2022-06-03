3241ce729ebec0434e143b92ffad2dd718175658
Jungle <1198581227@qq.com>
2022-06-03 16:50:08 +0800

    json和xml

---
+++ scratch/retrieve_data/json_xml.py
@@ -0,0 +1,26 @@
+import json
+
+from bs4 import BeautifulSoup
+
+serialized = """{"title": "Data Science Book", "author": "John Grus", "publicationYear": "2019","topics": ["data science", "machine learning", "artificial intelligence"]}"""
+
+# 将json反序列化成python对象
+deserialized = json.loads(serialized)
+assert deserialized["title"] == "Data Science Book"
+assert deserialized["author"] == "John Grus"
+assert deserialized["publicationYear"] == "2019"
+print("done")
+
+#
+xml_txt = """<Book>
+    <title>Data Science Book</title>
+    <author>John Grus</author>
+    <publicationYear>2019</publicationYear>
+    <topics>
+        <topic>data science</topic>
+        <topic>machine learning</topic>
+        <topic>artificial intelligence</topic>
+    </topics>
+</Book>"""
+
+beautiful_soup = BeautifulSoup(xml_txt, "xml")  # 看文档上应该是这么写的，但实际会报错，不清楚原因

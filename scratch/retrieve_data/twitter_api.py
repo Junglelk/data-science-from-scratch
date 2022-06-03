e2eb5073702500732955ae5c8c82dc1feeaadeaf
Jungle <1198581227@qq.com>
2022-06-03 23:16:51 +0800

    气死我了

---
+++ scratch/retrieve_data/twitter_api.py
@@ -0,0 +1,21 @@
+import os
+import webbrowser
+from twython import Twython
+
+# 获取的twitter开发者账号的key和secret，配置在环境变量中
+CONSUMER_KEY = os.environ.get('TWITTER_CONSUMER_KEY')
+CONSUMER_SECRET = os.environ.get('TWITTER_CONSUMER_SECRET')
+
+# 实例化客户端
+temp_client = Twython(CONSUMER_KEY, CONSUMER_SECRET)
+temp_creds = temp_client.get_authentication_tokens()
+
+url = temp_creds['auth_url']
+
+print(f"go visit {url} and get the PIN CODE and paste it below")
+webbrowser.open(url)
+PIN_CODE = input('Please enter the PIN CODE: ')
+
+auth_client = Twython(CONSUMER_KEY, CONSUMER_SECRET,
+                      temp_creds['oauth_token'], temp_creds['oauth_token_secret'])
+final_step = auth_client.get_authorized_tokens(PIN_CODE)

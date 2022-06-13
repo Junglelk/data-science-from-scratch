3f4ca6aadbae811de8c206c75707c792091016b7
Jungle <1198581227@qq.com>
2022-06-13 23:38:33 +0800

    多元回归分析-我感觉后面几天我需要点时间来看明白这里

--- scratch/multiple_regression.py
+++ scratch/multiple_regression.py
@@ -5,6 +5,10 @@
 
 from scratch.linear_algebra import dot, Vector
 from typing import List
+import random
+import tqdm
+from scratch.linear_algebra import vector_mean
+from scratch.gradient_descent import gradient_step
 
 
 # 模型为
@@ -27,3 +31,32 @@ y = 30
 beta = [4, 4, 4]  # 预测结果 = 4 + 8 + 12 = 24
 assert error(x, y, beta) == -6
 assert squared_error(x, y, beta) == 36
+
+
+# 梯度，此处即对x_i求导
+def sqerror_gradient(x: Vector, y: float, beta: Vector) -> Vector:
+    err = error(x, y, beta)
+    return [2 * err * x_i for x_i in x]
+
+
+assert sqerror_gradient(x, y, beta) == [-12, -24, -36]
+
+
+def least_squares_fit(xs: List[Vector],
+                      ys: List[float],
+                      learning_rate: float = 0.001,
+                      num_steps: int = 1000,
+                      batch_size: int = 1) -> Vector:
+    """
+    假设模型y = dot(x, beta)
+    找到使平方误差和最小的beta
+    """
+    # 先随机初始化权重
+    guess = [random.random() for _ in xs[0]]
+    for _ in tqdm.trange(num_steps, desc="least squares fit"):
+        for start in range(0, len(xs), batch_size):
+            batch_xs = xs[start:start + batch_size]
+            batch_ys = ys[start:start + batch_size]
+            gradient = vector_mean([sqerror_gradient(x, y, guess) for x, y in zip(batch_xs, batch_ys)])
+            guess = gradient_step(guess, gradient, -learning_rate)
+    return guess
